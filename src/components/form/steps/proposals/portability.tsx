9a98c37f2d0ea9379f67e97562351cd1e5a1d427
black-adm <mattheuscontato17@gmail.com>
2024-10-28 17:47:24 -0300

    fix: mobile update in carousel item

--- src/components/form/steps/proposals/portability.tsx
+++ src/components/form/steps/proposals/portability.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { CarouselItem } from "@/components/ui/carousel";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ContractsPortabilityDetails, Proposal } from "@/types/proposals";
 import { PersonData } from "./person-data";
 
@@ -17,85 +18,86 @@ export function PortabilityContent({
 }: PortabilityContentProps) {
   return (
     <CarouselItem key={proposal.id}>
-      <Card className="p-1">
-        <CardContent className="aspect-square flex flex-col items-center justify-between space-y-3 p-6">
-          <Badge className="bg-green-500 text-white">
-            Proposta {index + 1}
-          </Badge>
+      <ScrollArea className="h-80 py-6">
+        <Card className="p-1">
+          <CardContent className="aspect-square flex flex-col items-center justify-between space-y-3 p-6">
+            <Badge className="bg-green-500 text-white">
+              Proposta {index + 1}
+            </Badge>
 
-          <div className="text-center">
-            <span className="font-semibold">Econômia na redução</span>
-            <p className="text-2xl font-bold text-green-500">
-              {Number(
-                portability?.economiaReducao
-              ).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </p>
-          </div>
-
-          <div className="text-center">
-            <span className="font-semibold">Banco atual</span>
-            <p className="font-bold text-green-500">
-              {portability?.nomeBancoContratoAtual}
-            </p>
-          </div>
-
-          <div className="text-center">
-            <span className="font-semibold">Valor da Parcela</span>
-            <div className="flex flex-col">
-              <p className="text-xl font-bold text-destructive">
-                <small>Antes</small> {""}
+            <div className="text-center">
+              <span className="font-semibold">Econômia na redução</span>
+              <p className="text-2xl font-bold text-green-500">
                 {Number(
-                  portability?.antigoValorParcela
-                ).toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </p>
-              <p className="text-xl font-bold text-green-500">
-                <small>Depois</small> {""}
-                {Number(
-                  portability?.novoValorParcela
+                  portability?.economiaReducao
                 ).toLocaleString("pt-BR", {
                   style: "currency",
                   currency: "BRL",
                 })}
               </p>
             </div>
-          </div>
 
-          <div className="text-center">
-            <span className="font-semibold">Taxa de Juros</span>
-            <div className="flex flex-col">
-              <p className="text-xl font-bold text-destructive">
-                <small>Antes</small> {""}
-                {portability?.antigaTaxaJuros
-                  .toFixed(2)
-                  .replace(".", ",")
-                }% a.m.`
-              </p>
-
-              <p className="text-lg font-bold text-green-500">
-                <small>Depois</small> {""}
-                {portability?.novaTaxaJuros
-                  .toFixed(2)
-                  .replace(".", ",")
-                }% a.m.`
+            <div className="text-center">
+              <span className="font-semibold">Banco atual</span>
+              <p className="font-bold text-green-500">
+                {portability?.nomeBancoContratoAtual}
               </p>
             </div>
-          </div>
 
-          <div className="text-center">
-            <span className="font-semibold">Banco aprovado</span>
-            <p className="font-bold text-green-500">
-              {portability?.novoBancoAprovado}
-            </p>
-          </div>
-        </CardContent>
-      </Card>
+            <div className="text-center">
+              <span className="font-semibold">Valor da Parcela</span>
+              <div className="flex flex-col">
+                <p className="text-xl font-bold text-destructive">
+                  <small>Antes</small> {""}
+                  {Number(
+                    portability?.antigoValorParcela
+                  ).toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </p>
+                <p className="text-xl font-bold text-green-500">
+                  <small>Depois</small> {""}
+                  {Number(
+                    portability?.novoValorParcela
+                  ).toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </p>
+              </div>
+            </div>
 
+            <div className="text-center">
+              <span className="font-semibold">Taxa de Juros</span>
+              <div className="flex flex-col">
+                <p className="text-xl font-bold text-destructive">
+                  <small>Antes</small> {""}
+                  {portability?.antigaTaxaJuros
+                    .toFixed(2)
+                    .replace(".", ",")
+                  }% a.m.`
+                </p>
+
+                <p className="text-lg font-bold text-green-500">
+                  <small>Depois</small> {""}
+                  {portability?.novaTaxaJuros
+                    .toFixed(2)
+                    .replace(".", ",")
+                  }% a.m.`
+                </p>
+              </div>
+            </div>
+
+            <div className="text-center">
+              <span className="font-semibold">Banco aprovado</span>
+              <p className="font-bold text-green-500">
+                {portability?.novoBancoAprovado}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </ScrollArea>
       <PersonData personData={proposal.clienteDadosPessoais} />
     </CarouselItem>
   )
