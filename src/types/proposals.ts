9dca3bbe53caf5d736f1528e065dd7f74e60418b
black-adm <mattheuscontato17@gmail.com>
2024-11-13 08:35:57 -0300

    fix: update proposals type

--- src/types/proposals.ts
+++ src/types/proposals.ts
@@ -18,31 +18,33 @@ export interface PersonDetails {
 }
 
 export interface ContractsPortabilityDetails {
-  codigoBancoContratoAtual: number
-  nomeBancoContratoAtual: string;
-  numeroContratoAtual: string;
-  novoBancoAprovado: string;
   antigoSaldoDevedor: number;
   novoSaldoDevedor: number;
   antigoValorParcela: number;
   novoValorParcela: number;
+  economiaReducao: number;
+  codigoBancoContratoAtual: number
+  nomeBancoContratoAtual: string;
+  numeroContratoAtual: string;
+  bancoAprovado: string;
   antigaTaxaJuros: number;
   novaTaxaJuros: number;
-  economiaReducao: number;
+  valorEmprestado: number
   qtdParcelasPagas: number
 }
 
 export interface ContractsRefinancingDetails {
-  codigoBancoContratoAtual: number
-  nomeBancoContratoAtual: string
-  nomeBancoNovoContrato: string
-  numeroContratoAtual: string
   valorTroco: number
-  antigaTaxaJuros: number
-  novaTaxaJuros: number
   saldoDevedor: number
   saldoDevedorReduzido: number
   valorParcela: number
+  codigoBancoContratoAtual: number
+  nomeBancoContratoAtual: string
+  numeroContratoAtual: string
+  bancoAprovado: string
+  antigaTaxaJuros: number
+  novaTaxaJuros: number
+  valorEmprestado: number
   qtdParcelasPagas: number
 }
 
