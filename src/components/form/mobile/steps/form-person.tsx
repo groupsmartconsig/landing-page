5a406f92417774c855ef739645fe631adb3b6fc5
black-adm <mattheuscontato17@gmail.com>
2024-11-25 16:31:01 -0300

    fix: update validation phone number input

--- src/components/form/mobile/steps/form-person.tsx
+++ src/components/form/mobile/steps/form-person.tsx
@@ -17,7 +17,7 @@ import { z } from "zod";
 const formSchema = z.object({
   name: z.string().min(4, "O nome completo é um campo obrigatório."),
   cpf: z.string().min(14, "O CPF é obrigatório! Informe um CPF válido."),
-  phoneNumber: z.string().min(14, "Informe um telefone com DDD válido."),
+  phoneNumber: z.string().min(15, "Informe um telefone válido."),
 })
 
 type FormData = z.infer<typeof formSchema>;
