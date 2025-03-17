31b78244d027c3ea13d9053f5703c8c1424fe4d0
black-adm <mattheuscontato17@gmail.com>
2025-03-17 10:40:07 -0300

    fix: mask full name corrections

--- src/utils/mask/mask-full-name.ts
+++ src/utils/mask/mask-full-name.ts
@@ -1,6 +1,4 @@
 export const maskFullName = (value: string) => {
   return value
-    .replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, '')
-    .replace(/\s{2,}/g, ' ')
-    .trim();
+    .replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, '');
 };
