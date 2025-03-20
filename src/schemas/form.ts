f1bf96f689eaaaf1ec9c6cfdc8fef77922d5d8d7
black-adm <mattheuscontato17@gmail.com>
2025-03-20 09:15:46 -0300

    feat: cpf validation

--- src/schemas/form.ts
+++ src/schemas/form.ts
@@ -1,9 +1,19 @@
+import { isValidCpf } from "@/utils/mask/mask-cpf";
 import { z } from "zod";
 
 export type FormData = z.infer<typeof formSchema>;
 
 export const formSchema = z.object({
-  name: z.string().min(4, "O nome completo é um campo obrigatório."),
-  cpf: z.string().min(14, "O CPF é obrigatório! Informe um CPF válido."),
-  phoneNumber: z.string().min(15, "Informe um telefone válido."),
+  name: z
+    .string()
+    .min(4, "O nome completo é um campo obrigatório."),
+  cpf: z
+    .string()
+    .min(14, "O CPF é obrigatório! Informe um CPF válido.")
+    .refine((cpf: string) => isValidCpf(cpf), {
+      message: "Informe um CPF válido.",
+    }),
+  phoneNumber: z
+    .string()
+    .min(15, "Informe um telefone válido."),
 })
