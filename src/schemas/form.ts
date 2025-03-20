f9ddad8ab33212ce177a27b5d637481f0d7774f5
black-adm <mattheuscontato17@gmail.com>
2025-03-20 09:19:47 -0300

    fix: validate full name

--- src/schemas/form.ts
+++ src/schemas/form.ts
@@ -6,7 +6,14 @@ export type FormData = z.infer<typeof formSchema>;
 export const formSchema = z.object({
   name: z
     .string()
-    .min(4, "O nome completo é um campo obrigatório."),
+    .min(4, "O nome completo é um campo obrigatório.")
+    .refine(
+      value => {
+        const names = value.trim().split(" ");
+        return names.length >= 2;
+      },
+      "Você deve preencher o primeiro e o último nome."
+    ),
   cpf: z
     .string()
     .min(14, "O CPF é obrigatório! Informe um CPF válido.")
