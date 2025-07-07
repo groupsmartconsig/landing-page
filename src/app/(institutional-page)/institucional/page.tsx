6bf4f5460b3ec2badf05ceafd00f25f1c43a595a
black-adm <mattheuscontato17@gmail.com>
2025-07-07 10:22:49 -0300

    fix: company container and smartconsig squad section

--- src/app/(institutional-page)/institucional/page.tsx
+++ src/app/(institutional-page)/institucional/page.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import { InstitutionalHeader } from "./_components/header/header";
+import { InstitutionalCompanyContainer } from "./_components/sections/company-container";
 import { InstitutionalMainContainer } from "./_components/sections/main-container";
 import { InstitutionalPrimaryContainer } from "./_components/sections/primary-container";
 import { InstitutionalSecondaryContainer } from "./_components/sections/secondary-container";
@@ -12,6 +15,16 @@ export default function InstitutionalPage() {
       <InstitutionalSecondaryContainer />
       <InstitutionalMainContainer />
       <InstitutionalStatsContainer />
+      <InstitutionalCompanyContainer />
+      <div className="h-[580px] bg-black">
+        <Image
+          width={2148}
+          height={575}
+          src="/team.png"
+          alt="Equipe de colaboradores da Smartconsig"
+          className="object-cover"
+        />
+      </div>
     </div>
   )
 }
