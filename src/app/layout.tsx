3df06995f9bb01abd8ac00349cebb1427569dd48
Rafael da Conceição <rafael.conceicao@coderaw.io>
2025-07-29 09:44:33 -0300

    fix: use next/third-parties to register GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat, Open_Sans } from 'next/font/google'
 import { Toaster } from 'sonner'
+import {GoogleTagManager} from '@next/third-parties/google'
+
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -23,28 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KX2X2J86');
-            `,
-          }}
-        />
-      </head>
+      <GoogleTagManager gtmId='GTM-KX2X2J86'/>
       <body className={`${montserrat.className} ${openSans.variable} antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KX2X2J86"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <main>{children}</main>
         <Toaster position="top-center" richColors />
       </body>
